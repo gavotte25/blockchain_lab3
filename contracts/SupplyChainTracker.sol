7c94ae1c1ed9561917d00e3d97610f3a5e00c44a
gavotte25 <gavotte.tran@gmail.com>
2024-01-14 00:38:37 +0700

    - Implement basic Supply Chain Services

---
+++ contracts/SupplyChainTracker.sol
@@ -0,0 +1,184 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+contract SupplyChainTracker {
+
+    event ItemQuery(string name, string description, string unit, uint volume, uint price, string currentLocation, string managedBy, uint shipmentId);
+
+    event ShipmentQuery(uint etd, uint eta, uint atd, uint ata, string state, string origin, string destination, string currentLocation, string courier);
+
+    event ContractStateUpdated(string status);
+
+    enum State{ PREPARE, CREATED, SIGNED, DONE }
+
+    enum ShipmentState{ PREPARE, SIGNED, HANDED_OVER, DEPARTED, ARRIVED, DELIVERED}
+
+    struct Item {
+        string name;
+        string description;
+        string unit;
+        uint volume;
+        uint price;
+        uint shipmentId;
+    }
+
+    struct StakeHolder { // A party who can be sender, recipient, buy, seller, carier, customer...
+        address addr;
+        string name;
+    }
+
+    struct Shipment {
+        string currentLocation;
+        uint[] items;
+        string origin;
+        string destination;
+        StakeHolder courier;
+        uint etd;
+        uint eta;
+        uint atd;
+        uint ata;
+        ShipmentState state; 
+    }
+
+    struct TransactionItem {
+        uint productId;
+        uint price;
+        uint currency;
+        uint volume;
+    }
+
+    struct Transaction {
+        uint id;
+        address buyer;
+        address seller;
+        uint createdAt;
+        TransactionItem[] details;
+    }
+
+    StakeHolder contractOwner;
+    StakeHolder supplier;
+    uint itemCount = 0;
+    uint shipmentCount = 0;
+    mapping(uint => Item) items;
+    mapping(uint => Shipment) shipments;
+    State contractState = State.PREPARE;
+    uint createAt;
+    uint signedAt;
+    uint soonestEta;
+    uint latestEta;
+
+    constructor(string memory ownerName) {
+        contractOwner = StakeHolder(msg.sender, ownerName);
+    }
+
+    function addItem(string memory name, string memory description, string memory unit, uint volume, uint price) public {
+        require(msg.sender == contractOwner.addr, "Only contract owner can add item to order");
+        require(contractState == State.PREPARE, "Suply contract is created, cannot add new item");
+        itemCount++;
+        items[itemCount] = Item(name, description, unit, volume, price, 0);
+    }
+
+    function initSupplyContract(string memory supplierName, address supplierAddr, uint shortestAcceptibleEta, uint latestAcceptibleEta) public {
+        require(msg.sender == contractOwner.addr, "Only contract owner can create Sypply contract");
+        require(contractState == State.PREPARE, "Suply contract has been already created");
+        contractState = State.CREATED;
+        supplier = StakeHolder(supplierAddr, supplierName);
+        soonestEta = shortestAcceptibleEta;
+        latestEta = latestAcceptibleEta;
+    }
+
+    function signSupplyContract() public {
+        require(msg.sender == supplier.addr, "Not the supplierer of this contract");
+        require(contractState == State.CREATED, "Contract must be created and unsigned");
+        contractState = State.SIGNED;
+    }
+
+    function queryItemDetails(uint index) public {
+        Item storage item = items[index];
+        if (item.shipmentId == 0) {            
+            emit ItemQuery(item.name, item.description, item.unit, item.volume, item.price, "N/A", "N/A", 0);
+        } else {
+            Shipment storage shipment = shipments[item.shipmentId];
+            string memory manageBy = shipment.courier.name;
+            if (shipment.state == ShipmentState.PREPARE || shipment.state == ShipmentState.SIGNED) {
+                manageBy = supplier.name;
+            } else if (shipment.state == ShipmentState.DELIVERED) {
+                manageBy = contractOwner.name;
+            }
+            emit ItemQuery(item.name, item.description, item.unit, item.volume, item.price, shipment.currentLocation, manageBy, item.shipmentId);
+        }
+        
+    }
+
+    function createShipment(string memory courierName, address courierAddr, string memory currentLocation, uint[] memory shipmentItems, string memory origin, string memory destination, uint etd, uint eta) public returns(uint) {
+        require(msg.sender == supplier.addr, "Not the supplierer of this contract");
+        require(contractState == State.SIGNED, "Contract must be signed and not done yet");
+        shipmentCount++;
+        shipments[shipmentCount] = Shipment(currentLocation, shipmentItems, origin, destination, StakeHolder(courierAddr, courierName), etd, eta, 0, 0, ShipmentState.PREPARE);
+        for (uint i = 0; i < shipmentItems.length; i++) {
+            uint index = shipmentItems[i];
+            Item storage item = items[index];
+            item.shipmentId = shipmentCount;
+        }
+        return shipmentCount;
+    }
+
+    function signShipment(uint shipmentIndex) public {
+        Shipment storage shipment = shipments[shipmentIndex];
+        require(msg.sender == shipment.courier.addr, "Not the courier of this shipment");
+        require(shipment.state == ShipmentState.PREPARE, "Shipment has been already signed");
+        shipment.state = ShipmentState.SIGNED;
+    }
+
+    function transferFromSupplierToCourier(uint shipmentIndex) public {
+        Shipment storage shipment = shipments[shipmentIndex];
+        require(msg.sender == shipment.courier.addr, "Not the courier of this shipment");
+        require(shipment.state == ShipmentState.SIGNED, "Shipment must be signed and courier hasn't received items yet");
+        shipment.state = ShipmentState.HANDED_OVER;
+    }
+
+    function updateShipmentStatus(uint shipmentIndex, string memory currentLocation, uint state) public {
+        Shipment storage shipment = shipments[shipmentIndex];
+        require(msg.sender == shipment.courier.addr, "Not the courier of this shipment");
+        require(shipment.state == ShipmentState.SIGNED, "Shipment must be signed and courier hasn't received items yet");
+        require(state > 1, "This kind of state can only be updated by Supplier");
+        require(state < 5, "This kind of state can only be updated by Buyer");
+        if (state == 3) {
+            shipment.state = ShipmentState.DEPARTED;
+            shipment.atd = block.timestamp;
+        } else if (state == 4) {
+            require(keccak256(bytes(currentLocation)) == keccak256(bytes(shipment.destination)), "Destination mismatch");
+            shipment.state = ShipmentState.ARRIVED;
+            shipment.ata = block.timestamp;
+        } else {
+            shipment.state = ShipmentState.HANDED_OVER;
+        }
+        shipment.currentLocation = currentLocation;
+    }
+
+    function receiveFromCourier(uint shipmentIndex) public {
+        require(msg.sender == contractOwner.addr, "Only contract owner can create Supply contract");
+        Shipment storage shipment = shipments[shipmentIndex];
+        shipment.currentLocation = shipment.destination;
+        shipment.state = ShipmentState.DELIVERED;
+    }
+
+    function queryShipmenDetails(uint shipmentIndex) public {
+        Shipment storage shipment = shipments[shipmentIndex];
+        string memory state = "N/A";
+        if (shipment.state == ShipmentState.PREPARE) {
+            state = "PREPARE";
+        } else if (shipment.state == ShipmentState.SIGNED) {
+            state = "SIGNED";
+        } else if (shipment.state == ShipmentState.HANDED_OVER) {
+            state = "HANDED_OVER";
+        } else if (shipment.state == ShipmentState.DEPARTED) {
+            state = "DEPARTED";
+        } else if (shipment.state == ShipmentState.ARRIVED) {
+            state = "ARRIVED";
+        }  else if (shipment.state == ShipmentState.DELIVERED) {
+            state = "DELIVERED";
+        }
+        emit ShipmentQuery(shipment.etd, shipment.eta, shipment.atd, shipment.ata, state, shipment.origin, shipment.destination, shipment.currentLocation, shipment.courier.name);
+    }
+}
