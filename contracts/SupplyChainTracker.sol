fc96afa53060150e9e9cc79c1d04d7266f845261
stark4079 <xuanloc2018@gmail.com>
2024-01-19 12:03:28 +0700

    feat: get overall information, courier history

--- contracts/SupplyChainTracker.sol
+++ contracts/SupplyChainTracker.sol
@@ -7,6 +7,8 @@ contract SupplyChainTracker {
 
     event ShipmentQuery(uint etd, uint eta, uint atd, uint ata, string state, string origin, string destination, string currentLocation, string courier);
 
+    event CouierHistory(string name, address courierAdd, uint[] itemIndexs);
+
     event ContractStateUpdated(string status);
 
     enum State{ PREPARE, CREATED, SIGNED, DONE }
@@ -40,27 +42,18 @@ contract SupplyChainTracker {
         ShipmentState state; 
     }
 
-    struct TransactionItem {
-        uint productId;
-        uint price;
-        uint currency;
-        uint volume;
-    }
-
-    struct Transaction {
-        uint id;
-        address buyer;
-        address seller;
-        uint createdAt;
-        TransactionItem[] details;
-    }
-
     StakeHolder contractOwner;
     StakeHolder supplier;
     uint itemCount = 0;
     uint shipmentCount = 0;
+    uint courierCount = 0;
+    uint finishedCount = 0;
     mapping(uint => Item) items;
     mapping(uint => Shipment) shipments;
+    mapping(address => uint[]) courierHistory;
+    mapping(address => StakeHolder) couriers;
+    mapping(uint => bool) isFinishedShipment; // view as queue
+
     State contractState = State.PREPARE;
     uint createAt;
     uint signedAt;
@@ -74,8 +67,10 @@ contract SupplyChainTracker {
     function addItem(string memory name, string memory description, string memory unit, uint volume, uint price) public {
         require(msg.sender == contractOwner.addr, "Only contract owner can add item to order");
         require(contractState == State.PREPARE, "Suply contract is created, cannot add new item");
-        itemCount++;
         items[itemCount] = Item(name, description, unit, volume, price, 0);
+        isFinishedShipment[finishedCount] = false;
+        itemCount++;
+        finishedCount++;   
     }
 
     function initSupplyContract(string memory supplierName, address supplierAddr, uint shortestAcceptibleEta, uint latestAcceptibleEta) public {
@@ -113,8 +108,23 @@ contract SupplyChainTracker {
     function createShipment(string memory courierName, address courierAddr, string memory currentLocation, uint[] memory shipmentItems, string memory origin, string memory destination, uint etd, uint eta) public returns(uint) {
         require(msg.sender == supplier.addr, "Not the supplierer of this contract");
         require(contractState == State.SIGNED, "Contract must be signed and not done yet");
-        shipmentCount++;
+        bool valid_flag = true;
+        for (uint i = 0; i < shipmentItems.length; i++) {
+            uint index = shipmentItems[i];
+            Item storage item = items[index];
+            if (item.shipmentId != 0){
+                valid_flag = false;
+                break;
+            }
+        }
+        require(valid_flag, "Item already be captured");
+        // add courier
+        courierHistory[courierAddr] = shipmentItems;
+        couriers[courierAddr] = StakeHolder(courierAddr, courierName);
+        courierCount ++;
+        // add shipment
         shipments[shipmentCount] = Shipment(currentLocation, shipmentItems, origin, destination, StakeHolder(courierAddr, courierName), etd, eta, 0, 0, ShipmentState.PREPARE);
+        shipmentCount++;
         for (uint i = 0; i < shipmentItems.length; i++) {
             uint index = shipmentItems[i];
             Item storage item = items[index];
@@ -161,6 +171,12 @@ contract SupplyChainTracker {
         Shipment storage shipment = shipments[shipmentIndex];
         shipment.currentLocation = shipment.destination;
         shipment.state = ShipmentState.DELIVERED;
+        StakeHolder storage courier = shipment.courier;
+        uint [] storage itemIds = courierHistory[courier.addr];
+        for(uint i=0; i < itemIds.length; i++){
+            finishedCount --;
+            delete isFinishedShipment[itemIds[i]];
+        }
     }
 
     function queryShipmenDetails(uint shipmentIndex) public {
@@ -180,5 +196,41 @@ contract SupplyChainTracker {
             state = "DELIVERED";
         }
         emit ShipmentQuery(shipment.etd, shipment.eta, shipment.atd, shipment.ata, state, shipment.origin, shipment.destination, shipment.currentLocation, shipment.courier.name);
+    }
+
+    function queryCourierHolding(address courierAddr) public{
+        // For getting courier name
+        StakeHolder storage courier = couriers[courierAddr];
+        // For get holding items
+        uint[] storage itemIds = courierHistory[courierAddr];
+        emit CouierHistory(courier.name, courierAddr, itemIds);
+    }
+
+    function getItemCount() public view returns (uint){
+    return itemCount;
+    }
+
+    function getShipmentCount() public view returns (uint){
+        return shipmentCount;
+    }
+    
+    function getCourierCount() public view returns (uint){
+        return courierCount;
+    }
+
+    function getOwner() public view returns (address){
+        return contractOwner.addr;
+    }
+
+    function getSupplier() public view returns (address){
+        return supplier.addr;
+    }
+
+    function checkSatisfiedContrast() public view returns (bool){
+        bool is_satisfied = true;
+        if (finishedCount >0){
+            return false;
+        }
+        return is_satisfied;
     }
 }
